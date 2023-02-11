895f792f1dfc4c344672f5cf895a33c42386ecd5
aleecmp <ale.cmpb@gmail.com>
2023-02-11 18:19:28 -0300

    fix: add mysql2 package

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,9 +1,9 @@
-import mysql from "mysql";
+import mysql2 from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql2.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
