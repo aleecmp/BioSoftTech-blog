b03f8e128c07e0d740e7c20ac8cf4b38ae5e657b
aleecmp <ale.cmpb@gmail.com>
2023-02-11 15:08:29 -0300

    fix db config

--- api/src/app.js
+++ api/src/app.js
@@ -1,18 +1,18 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 
-import authRoutes from "./routes/auth.js";
-import postsRoutes from "./routes/posts.js";
-import userRoutes from "./routes/users.js";
+import authRoutes from './routes/auth.js';
+import postsRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
-// import uploadRoutes from "./routes/upload.js";
-
-import multer from "multer";
+import multer from 'multer';
 
 const app = express();
 
 // MIDDLEWARES
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
@@ -20,7 +20,7 @@ app.use(morgan('dev'));
 // ROUTES
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../client/public/upload");
+    cb(null, '../client/public/upload');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
@@ -29,14 +29,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.post("/api/upload", upload.single("file"), function (req, res) {
+app.post('/api/upload', upload.single('file'), function (req, res) {
   const file = req.file;
   res.status(200).json(file.filename);
 });
 
 // app.use("/api/upload", uploadRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postsRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postsRoutes);
+app.use('/api/users', userRoutes);
 
 export default app;
--- api/src/config/db.js
+++ api/src/config/db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "blog",
+  database: process.env.DB_NAME
 });
 
 export default db;
